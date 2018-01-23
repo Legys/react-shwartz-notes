8c7c058ee68274507b566d7eafa6865b1fe31b22
Alexandr Dubrovin <legys5@yandex.ru>
2018-01-23 17:44:15 +0200

    done routing task

--- routing/src/containers/Course/Course.js
+++ routing/src/containers/Course/Course.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  state = {
+    id: null,
+    title: null
+  }
+  componentDidMount() {
+    this.setId()
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.id !== this.props.match.params.id) {
+      this.setId()
     }
+  }
+  setId() {
+    const query = new URLSearchParams(this.props.location.search);
+    for (let param of query.entries()) {
+      if (param[0] === 'title') {
+        this.setState({ title: param[1] })
+      }
+    }
+    this.setState({ id: this.props.match.params.id })
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.state.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
